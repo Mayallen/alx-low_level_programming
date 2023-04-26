564ffe129ba896cda7111dfa9fb0e70e2141ab9c
Mayallen <marryallen2338@gmail.com>
2023-04-26 10:12:02 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,17 +11,29 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (; n > 98; n++)
+		for (; n >= 98; n--)
 		{
-			printf("%d,", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
 		}
 	}
 	else if (n < 98)
 	{
-		for (; n < 98; n--)
+		for (; n < 98; n++)
 		{
 			printf("%d,", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
 		}
 	}
-	printf("%d\n", n);
+	else
+	{
+		printf("%d", n);
+	}
+	printf("n");
 }
